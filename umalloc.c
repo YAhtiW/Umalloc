c48091a298c857573ecc6122861ab1a977066ff4
YAhtiW <102620528+YAhtiW@users.noreply.github.com>
2022-11-21 23:06:14 -0500

    Create umalloc.c

---
+++ umalloc.c
@@ -0,0 +1,58 @@
+#include <assert.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int global_base = 0;
+struct block_meta {
+  size_t size;
+  struct block_meta *next;
+  int free;
+  int magic; // For debugging only. TODO: remove this in non-debug mode.
+};
+
+void *malloc(size_t size) {
+  struct block_meta *block;
+  // TODO: align size?
+
+  if (size <= 0) {
+    return NULL;
+  }
+
+  if (!global_base) { // First call.
+    block = request_space(NULL, size);
+    if (!block) {
+      return NULL;
+    }
+    global_base = block;
+  } else {
+    struct block_meta *last = global_base;
+    block = find_free_block(&last, size);
+    if (!block) { // Failed to find free block.
+      block = request_space(last, size);
+      if (!block) {
+        return NULL;
+      }
+    } else {      // Found free block
+      // TODO: consider splitting block here.
+      block->free = 0;
+      block->magic = 0x77777777;
+    }
+  }
+
+  return(block+1);
+}
+
+void free(void *ptr) {
+  if (!ptr) {
+    return;
+  }
+
+  // TODO: consider merging blocks once splitting blocks is implemented.
+  struct block_meta* block_ptr = get_block_ptr(ptr);
+  assert(block_ptr->free == 0);
+  assert(block_ptr->magic == 0x77777777 || block_ptr->magic == 0x12345678);
+  block_ptr->free = 1;
+  block_ptr->magic = 0x55555555;
+}
